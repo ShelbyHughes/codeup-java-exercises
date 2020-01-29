f1fde8f0283016abc2f914184cdf37578f913236
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-01-29 11:00:42 -0600

    I think I did these exercises correctly

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,28 @@
 public class StringExercise {
     public static void main(String args[]){
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
 
+//        We don't need no education
+//        We don't need no thought control
+
+        String pinkFloyd = "We don't need no ";
+        System.out.println(pinkFloyd + "education");
+        System.out.println(pinkFloyd + "thought control");
+
+//        Check "this" out!, "s inside of "s!
+
+        String attempt = "Check \"this\" out, ";
+        System.out.println(attempt + "\"s inside of \"s!");
+
+//        In windows, the main drive is usually C:\
+
+        String windows = "In windows, the main drive is usually ";
+        System.out.println(windows + "C:\\");
+
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+        String amazing = "I can do backslashes \\, double backslashes \\\\, ";
+        System.out.println(amazing + "and the amazing triple backslash \\\\\\!");
     }
 }
